fc645eed4f17c948b6f24d3a38fb3d0a78cda0cc
Patrick Withams <pwithams@gmail.com>
2022-11-24 16:39:11 -0700

    v0.1.7

--- dexml/__init__.py
+++ dexml/__init__.py
@@ -6,7 +6,7 @@ from dexml.constants import Status
 from dexml.exceptions import ParseError, RenderError, XmlError
 from dexml.model import Model
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 
 __all__ = [
